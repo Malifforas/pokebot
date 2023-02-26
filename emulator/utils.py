da9f6708f29c820b30387cffe8a6de398e54994d
Malifforas <122689565+Malifforas@users.noreply.github.com>
2023-02-25 21:50:19 -0500

    Add files via upload

--- emulator/utils.py
+++ emulator/utils.py
@@ -1,10 +1,13 @@
 import numpy as np
 from PIL import Image
-
-from emulator import Emulator
+from .emulator import Emulator
 
 
 class Util:
+    @staticmethod
+    def get_screen_dimensions(screen):
+        return screen.shape[0], screen.shape[1]
+
     @staticmethod
     def crop_screen(screen, top, bottom, left, right):
         return screen[top:bottom, left:right]
@@ -13,15 +16,18 @@ class Util:
     def resize_screen(screen, width, height):
         return np.array(Image.fromarray(screen).resize((width, height), resample=Image.BOX))
 
+
 def load_rom(emulator: Emulator, rom_path: str) -> None:
     with open(rom_path, "rb") as f:
         rom = f.read()
     emulator.load_rom(rom)
 
+
 def reset_emulator(emulator: Emulator) -> None:
     buttons_to_press = ["POWER", "A", "START", "A", "A", "A", "A", "A", "A"]
     for button in buttons_to_press:
         emulator.press_button(button)
+
 
 def get_game_state(emulator: Emulator) -> np.ndarray:
     state = emulator.get_screen()
@@ -30,8 +36,10 @@ def get_game_state(emulator: Emulator) -> np.ndarray:
     state = state / 255.0
     return state
 
+
 def get_action_space() -> list:
     return ["UP", "DOWN", "LEFT", "RIGHT", "A", "B", "START", "SELECT"]
+
 
 def get_reward(action, prev_state, curr_state):
     """
@@ -46,6 +54,7 @@ def get_reward(action, prev_state, curr_state):
         float: The reward for taking the action.
     """
     # Implementation details omitted for brevity
+
 
 def perform_action(emulator: Emulator, action: str) -> None:
     emulator.press_button(action)
