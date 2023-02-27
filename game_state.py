f516361bf2ca27a07d077247d3c9437ed40a6b3e
Malifforas <122689565+Malifforas@users.noreply.github.com>
2023-02-27 01:57:41 -0500

    Add files via upload

---
+++ game_state.py
@@ -0,0 +1,28 @@
+class GameState:
+    def __init__(self):
+        self.current_pokemon = None
+        self.opponent_pokemon = None
+        self.battle_over = False
+from typing import List, Dict, Any
+
+class Pokemon:
+    def __init__(self, data: Dict[str, Any]):
+        self.name = data['name']
+        self.hp = data['hp']
+        self.max_hp = data['max_hp']
+        self.status = data['status']
+        self.moves = data['moves']
+        self.active = data['active']
+
+class Opponent:
+    def __init__(self, data: Dict[str, Any]):
+        self.name = data['name']
+        self.hp = data['hp']
+        self.max_hp = data['max_hp']
+        self.status = data['status']
+
+class GameState:
+    def __init__(self, player_data: Dict[str, Any], opponent_data: Dict[str, Any]):
+        self.player = Pokemon(player_data)
+        self.opponent = Opponent(opponent_data)
+        
